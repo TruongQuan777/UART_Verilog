b924ae0e04a95886d6d9ac0efcd695eb32fd9faa
Quan777 <106910746+TruongQuan777@users.noreply.github.com>
2025-10-05 01:58:36 +0800

    Create baudrate_generator.v

---
+++ src/baudrate_generator.v
@@ -0,0 +1,20 @@
+module baudrate_generator(
+    input clk,
+    output tick
+);
+    parameter baud_rate=9600,
+			  clk_rate=50000000,
+			  oversample_rate=16,
+			  max_counter=clk_rate/(baud_rate*oversample_rate),
+  			  counter_width=$clog2(max_counter);
+    
+    reg [counter_width-1:0] counter; 
+    
+    always @(posedge clk)
+        begin
+          if(counter==max_counter-1) counter<=0;
+          else counter<=counter+1;            
+        end
+    
+  assign tick=(counter==max_counter-1);
+endmodule
